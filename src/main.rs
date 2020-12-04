31eee2a282f09433a4215ab3f5f7a4c897a81750
flippingbitss <matharumanpreet00@gmail.com>
2020-12-04 08:52:06 -0500

    player_state bfs - 80k nodes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{collections::{HashMap, HashSet, LinkedList, VecDeque}, io::{self, BufRead}, time::{Duration, Instant}};
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    io::{self, BufRead},
+    time::{Duration, Instant},
+};
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => {
@@ -21,7 +25,7 @@ mod vec4 {
     }
 
     impl Vec4 {
-        pub fn new(x: i32, y: i32, z: i32, w: i32) -> Vec4 {
+        pub const fn new(x: i32, y: i32, z: i32, w: i32) -> Vec4 {
             Vec4 { x, y, z, w }
         }
 
@@ -100,11 +104,13 @@ mod vec4 {
 
 use vec4::Vec4;
 
+type Id = usize;
+
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 enum Action {
-    Learn(i32),
-    Brew(i32),
-    Cast(i32),
+    Learn(Id),
+    Brew(Id),
+    Cast(Id),
     Rest,
     Wait,
 }
@@ -123,19 +129,17 @@ impl ToString for Action {
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
 struct Spell {
-    pub id: i32,
-    pub is_repeatable: bool,
-    pub is_castable: bool,
+    pub id: Id,
     pub delta: Vec4,
+    pub is_repeatable: bool,
 }
 
 impl Spell {
-    fn new(id: i32, is_repeatable: bool, is_castable: bool, delta: Vec4) -> Self {
+    fn new(id: usize, delta: Vec4) -> Self {
         Self {
             id,
-            is_repeatable,
-            is_castable,
             delta,
+            is_repeatable: !delta.is_non_neg(),
         }
     }
 
@@ -146,34 +150,34 @@ impl Spell {
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
 struct TomeSpell {
-    pub id: i32,
-    pub is_repeatable: bool,
-    pub tome_index: i32,
-    pub tax_count: i32,
+    pub id: Id,
     pub delta: Vec4,
+    pub tome_index: usize,
 }
 
 impl TomeSpell {
-    fn new(id: i32, is_repeatable: bool, tome_index: i32, tax_count: i32, delta: Vec4) -> Self {
+    fn new(id: Id, delta: Vec4, tome_index: usize) -> Self {
         Self {
             id,
-            is_repeatable,
-            tome_index,
-            tax_count,
             delta,
+            tome_index,
         }
+    }
+
+    fn is_repeatable(&self) -> bool {
+        !self.delta.is_non_neg()
     }
 }
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
 struct Order {
-    id: i32,
+    id: Id,
     price: i32,
     delta: Vec4,
 }
 
 impl Order {
-    fn new(id: i32, price: i32, delta: Vec4) -> Self {
+    fn new(id: Id, price: i32, delta: Vec4) -> Self {
         Self { id, price, delta }
     }
 
@@ -208,32 +212,153 @@ impl MagicTome {
 }
 
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
-struct Player {
+struct PlayerState {
     score: i32,
     inventory: Vec4,
-    spells: Vec<Spell>,
+    brew_count: i32,
+    available_spells: Vec<usize>,
 }
 
-impl Player {
-    fn new(score: i32, inventory: Vec4, spells: Vec<Spell>) -> Self {
+impl PlayerState {
+    fn new(score: i32, brew_count: i32, inventory: Vec4, available_spells: Vec<usize>) -> Self {
         Self {
             score,
+            brew_count,
             inventory,
-            spells,
+            available_spells,
+        }
+    }
+
+    fn evaluate(&self) -> f64 {
+        let mut score = 0;
+        let inv = self.inventory;
+        score += self.score;
+        score += inv.x + inv.y * 2 + inv.z * 3 + inv.w * 4;
+        score as f64
+    }
+
+    fn can_afford_spell(&self, ingredients: Vec4) -> bool {
+        (ingredients + self.inventory).is_non_neg()
+    }
+
+    // fn get_spell(&self, id: Id) -> Vec4 {
+    //     match id {
+    //         0..=3 => PRIMITIVE_SPELLS[id],
+    //         4..=41 => TOME[id - 4],
+    //         _ => unreachable!()
+    //     }
+    // }
+
+    fn get_possible_actions(&self, game_state: &GameState, turn: usize) -> Vec<Action> {
+        let mut actions = Vec::new();
+
+        for order in game_state.orders.iter() {
+            if order.can_be_fulfilled_by(self.inventory) {
+                actions.push(Action::Brew(order.id));
+            }
+        }
+
+        if turn < 5 {
+            for (idx, spell) in game_state.tome.spells.iter().enumerate() {
+                if self.inventory.x >= idx as i32 {
+                    actions.push(Action::Learn(spell.id));
+                }
+            }
+        }
+
+        for &id in self.available_spells.iter() {
+            if let Some(spell) = game_state.my_spells.iter().find(|s| s.id == id) {
+                if self.can_afford_spell(spell.delta) {
+                    actions.push(Action::Cast(id));
+                }
+            } else {
+                unreachable!();
+            }
+        }
+
+        let have_exhausted_spells = self.available_spells.len() != game_state.my_spells.len();
+
+        if have_exhausted_spells {
+            actions.push(Action::Rest);
+        }
+        // dbg!(&actions);
+        actions
+    }
+
+    fn apply(&mut self, game_state: &mut GameState, action: Action) {
+        match action {
+            Action::Brew(id) => {
+                let order_idx = game_state
+                    .orders
+                    .iter()
+                    .position(|o| o.id == id && o.can_be_fulfilled_by(self.inventory))
+                    .unwrap();
+                let order = game_state.orders[order_idx];
+
+                self.inventory += order.delta;
+                self.score += order.price;
+                self.brew_count += 1;
+
+                game_state.orders.remove(order_idx);
+            }
+            Action::Cast(id) => {
+                if let Some(spell) = game_state.my_spells.iter().find(|&s| s.id == id) {
+                    if let Some(idx) = self.available_spells.iter().position(|&idx| idx == id) {
+                        self.inventory += spell.delta;
+                        self.available_spells.remove(idx);
+                    } else {
+                        panic!("tried removing already unavailable spell");
+                    }
+                }
+                // dbg!(&self.me.spells);
+            }
+            Action::Learn(id) => {
+                if let Some(spell_idx) = game_state.tome.spells.iter().position(|s| s.id == id) {
+                    let spell = game_state.tome.spells[spell_idx];
+                    let delta = Vec4::new(spell.tome_index as i32, 0, 0, 0);
+
+                    if (self.inventory - delta).is_non_neg() {
+                        let new_spell = Spell::new(spell.id + 1000, spell.delta);
+                        game_state.my_spells.push(new_spell);
+                        game_state.tome.remove_spell(spell)
+                    }
+                }
+            }
+            Action::Rest => {
+                self.available_spells = game_state.my_spells.iter().map(|s| s.id).collect()
+            }
+            Action::Wait => {}
         }
     }
 }
 
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
-struct State {
-    me: Player,
+struct GameState {
+    me: PlayerState,
+    my_spells: Vec<Spell>,
+    enemy: PlayerState,
+    enemy_spells: Vec<Spell>,
     orders: Vec<Order>,
     tome: MagicTome,
 }
 //
-impl State {
-    fn new(me: Player, tome: MagicTome, orders: Vec<Order>) -> Self {
-        Self { me, tome, orders }
+impl GameState {
+    fn new(
+        me: PlayerState,
+        my_spells: Vec<Spell>,
+        enemy: PlayerState,
+        enemy_spells: Vec<Spell>,
+        orders: Vec<Order>,
+        tome: MagicTome,
+    ) -> Self {
+        Self {
+            me,
+            my_spells,
+            enemy,
+            enemy_spells,
+            orders,
+            tome,
+        }
     }
 
     fn get_possible_actions(&self) -> Vec<Action> {
@@ -252,15 +377,15 @@ impl State {
             }
         }
 
-        for spell in self.me.spells.iter() {
-            if spell.is_castable {
-                if spell.can_be_afforded_by(self.me.inventory) {
-                    actions.push(Action::Cast(spell.id));
-                }
-            } else {
-                can_use_rest = true;
-            }
-        }
+        // for spell in self.me.spells.iter() {
+        //     if spell.is_castable {
+        //         if spell.can_be_afforded_by(self.me.inventory) {
+        //             actions.push(Action::Cast(spell.id));
+        //         }
+        //     } else {
+        //         can_use_rest = true;
+        //     }
+        // }
 
         if can_use_rest {
             actions.push(Action::Rest);
@@ -281,35 +406,35 @@ impl State {
 
                 self.me.inventory += order.delta;
                 self.me.score += order.price;
+                self.me.brew_count += 1;
 
                 self.orders.remove(order_idx);
             }
             Action::Cast(id) => {
-                if let Some(spell) = self.me.spells.iter_mut().find(|s| s.id == id) {
-                    if spell.is_castable {
+                if let Some(spell) = self.my_spells.iter().find(|&s| s.id == id) {
+                    if let Some(idx) = self.me.available_spells.iter().position(|&idx| idx == id) {
                         self.me.inventory += spell.delta;
-                        spell.is_castable = false;
+                        self.me.available_spells.remove(idx);
+                    } else {
+                        panic!("tried removing already unavailable spell");
                     }
-                    // dbg!(&self.me.spells);
                 }
+                // dbg!(&self.me.spells);
             }
             Action::Learn(id) => {
                 if let Some(spell_idx) = self.tome.spells.iter().position(|s| s.id == id) {
                     let spell = self.tome.spells[spell_idx];
-                    let delta = Vec4::new(spell.tome_index, 0, 0, 0);
+                    let delta = Vec4::new(spell.tome_index as i32, 0, 0, 0);
 
                     if (self.me.inventory - delta).is_non_neg() {
-                        let new_spell =
-                            Spell::new(spell.id + 1000, spell.is_repeatable, true, spell.delta);
-                        self.me.spells.push(new_spell);
+                        let new_spell = Spell::new(spell.id + 1000, spell.delta);
+                        self.my_spells.push(new_spell);
                         self.tome.remove_spell(spell)
                     }
                 }
             }
             Action::Rest => {
-                for spell in self.me.spells.iter_mut() {
-                    spell.is_castable = true;
-                }
+                self.me.available_spells = self.my_spells.iter().map(|s| s.id).collect()
             }
             Action::Wait => {}
         }
@@ -339,8 +464,8 @@ impl State {
         for _ in 0..action_count as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
-            let inputs = input_line.split(" ").collect::<Vec<_>>();
-            let action_id = parse_input!(inputs[0], i32); // the unique ID of this spell or recipe
+            let inputs = input_line.split(' ').collect::<Vec<_>>();
+            let action_id = parse_input!(inputs[0], Id); // the unique ID of this spell or recipe
             let action_type = inputs[1].trim().to_string(); // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
 
             let x = parse_input!(inputs[2], i32); // tier-0 ingredient change
@@ -359,35 +484,46 @@ impl State {
                     orders.push(Order::new(action_id, price, delta));
                 }
                 "CAST" => {
-                    my_spells.push(Spell::new(action_id, repeatable, castable, delta));
+                    my_spells.push(Spell::new(action_id, delta));
                 }
                 "OPPONENT_CAST" => {
-                    enemy_spells.push(Spell::new(action_id, repeatable, castable, delta));
+                    enemy_spells.push(Spell::new(action_id, delta));
                 }
                 "LEARN" => {
-                    tome.push(TomeSpell::new(
-                        action_id, repeatable, tome_index, tax_count, delta,
-                    ));
+                    tome.push(TomeSpell::new(action_id, delta, tome_index as usize));
                 }
                 _ => {}
             }
         }
 
-        // for i in 0..2 as usize {
-        let mut input_line = String::new();
-        // io::stdin()
-        io::stdin().read_line(&mut input_line).unwrap();
-        let inputs = input_line.split(' ').collect::<Vec<_>>();
-        let x = parse_input!(inputs[0], i32); // tier-0 ingredients in inventory
-        let y = parse_input!(inputs[1], i32);
-        let z = parse_input!(inputs[2], i32);
-        let w = parse_input!(inputs[3], i32);
-        let score = parse_input!(inputs[4], i32); // amount of rupees
+        let read_player = |spells: &Vec<Spell>| {
+            let mut input_line = String::new();
+            // io::stdin()
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(' ').collect::<Vec<_>>();
+            let x = parse_input!(inputs[0], i32); // tier-0 ingredients in inventory
+            let y = parse_input!(inputs[1], i32);
+            let z = parse_input!(inputs[2], i32);
+            let w = parse_input!(inputs[3], i32);
+            let score = parse_input!(inputs[4], i32); // amount of rupees
+            PlayerState::new(
+                score,
+                0,
+                Vec4::new(x, y, z, w),
+                spells.iter().map(|s| s.id).collect(),
+            )
+        };
 
-        let me = Player::new(score, Vec4::new(x, y, z, w), my_spells);
-        // }
-
-        State::new(me, MagicTome::new(tome), orders)
+        let me = read_player(&my_spells);
+        let enemy = read_player(&enemy_spells);
+        GameState::new(
+            me,
+            my_spells,
+            enemy,
+            enemy_spells,
+            orders,
+            MagicTome::new(tome),
+        )
     }
 }
 
@@ -399,34 +535,36 @@ impl Bot {
         Self {}
     }
 
-    fn bfs(self, start_instant: &Instant, state: &State) -> Vec<Action> {
-        const MAX_DURATION: Duration = Duration::from_millis(1000);
-        let mut queue = LinkedList::<State>::new();
-        // let mut queue = VecDeque::<State>::new();
-        let mut visited = HashSet::<State>::new();
-        let mut predecessor = HashMap::<State, State>::new();
-        let mut pred_action = HashMap::<State, Action>::new();
+    fn bfs(self, start_instant: &Instant, game_state: &mut GameState, turn: usize) -> Vec<Action> {
+        const MAX_DURATION: Duration = Duration::from_millis(40);
+        let state = game_state.me.clone();
+
+        let mut queue = VecDeque::<PlayerState>::new();
+        let mut visited = HashSet::<PlayerState>::new();
+
+        let mut predecessor = HashMap::<PlayerState, PlayerState>::new();
+        let mut pred_action = HashMap::<PlayerState, Action>::new();
 
         let initial_state = state.clone();
         queue.push_back(state.clone());
         pred_action.insert(state.clone(), Action::Wait);
         visited.insert(state.clone());
         let mut iterations = 0;
+        let mut best_state = Option::None;
+        let mut best_score = f64::MIN;
+
         while let Some(current_state) = queue.pop_front() {
             if start_instant.elapsed() > MAX_DURATION {
-                // time over
-                break;
-            }
-            iterations += 1;
-
-            if current_state.find_brewable_order().is_some() {
                 let mut path = Vec::<Action>::new();
-                let mut curr_state = &current_state;
-                while curr_state != &initial_state {
-                    let action = pred_action.get(curr_state).expect("pred action not found");
-                    let last_state = predecessor.get(curr_state).expect("prev state not found");
+                let mut curr_state = best_state.expect("best state missing");
+                // let mut curr_state = current_state;
+                // dbg!(&curr_state);
+
+                while curr_state != initial_state {
+                    let action = pred_action.get(&curr_state).expect("pred action not found");
+                    let last_state = predecessor.get(&curr_state).expect("prev state not found");
                     path.push(*action);
-                    curr_state = last_state;
+                    curr_state = last_state.clone();
                 }
 
                 println!("{} game states visited", iterations);
@@ -435,9 +573,16 @@ impl Bot {
 
             let curr_state = &current_state;
 
-            for action in current_state.get_possible_actions() {
+            let score = curr_state.evaluate();
+            if score > best_score {
+                best_score = score;
+                best_state = Some(curr_state.clone())
+            }
+
+            for action in current_state.get_possible_actions(&game_state, turn) {
                 let mut next = curr_state.clone();
-                next.apply(action);
+                next.apply(game_state, action);
+                iterations += 1;
 
                 if !visited.contains(&next) {
                     queue.push_back(next.clone());
@@ -453,16 +598,20 @@ impl Bot {
         Vec::new()
     }
 
-    fn think(self, start_instant: &Instant, state: &State) -> Action {
+    fn think(self, start_instant: &Instant, state: &mut GameState, turn: usize) -> Action {
         if let Some(order) = state.find_brewable_order() {
             return Action::Brew(order.id);
         }
 
-        let actions: Vec<Action> = self.bfs(start_instant, state).into_iter().rev().collect();
+        let actions: Vec<Action> = self
+            .bfs(start_instant, state, turn)
+            .into_iter()
+            .rev()
+            .collect();
 
-        // for action in actions.iter() {
-        //     println!("{}", action.to_string());
-        // }
+        for action in actions.iter() {
+            println!("{}", action.to_string());
+        }
 
         if let Some(&action) = actions.first() {
             return action;
@@ -471,51 +620,28 @@ impl Bot {
         // println!("Time took: {} ms", start_instant.elapsed().as_millis());
 
         println!("no actions computed, taking first possible action");
-
-        state.get_possible_actions().first().cloned().unwrap()
+        Action::Wait
+        // state.get_possible_actions().first().cloned().unwrap()
     }
 }
 
 fn main() {
     let bot = Bot::new();
 
-    let state = State::read_from_io();
-
-    let mut total_duration = Duration::new(0, 0);
-    for _ in 0..100 {
-        let turn_state = state.clone();
-       // let turn_state = State::read_from_io();
-
-        let start_instant = Instant::now();
-        let action = bot.think(&start_instant, &turn_state);
-        total_duration += start_instant.elapsed();
-
-        println!("Action {}", action.to_string());
-    }
-
-     println!("Average time taken: {:?}", total_duration / 100)
-}
-
-fn main2() {
-    let bot = Bot::new();
-
-    let state = State::read_from_io();
+    let state = GameState::read_from_io();
     let mut turn = 0;
     let mut total_duration = Duration::new(0, 0);
-    for _ in 0..100 {
-        let turn_state = state.clone();
+
+    for _ in 0..10 {
+        let mut turn_state = state.clone();
+        // let turn_state = State::read_from_io();
+
         let start_instant = Instant::now();
-
-        let action = match turn {
-            0..=6 => Action::Learn(turn_state.tome.spells.first().unwrap().id),
-            _ => bot.think(&start_instant, &turn_state),
-        };
-
-        // let action = bot.think(&start_instant, &turn_state);
+        let action = bot.think(&start_instant, &mut turn_state, turn);
         total_duration += start_instant.elapsed();
         turn += 1;
         println!("Action {}", action.to_string());
     }
 
-    println!("Average time taken: {:?}", total_duration / 101)
+    println!("Average time taken: {:?}", total_duration / 100)
 }
